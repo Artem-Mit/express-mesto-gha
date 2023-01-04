7974156849ad5f80955484ea30bee45110cba875
Artem <artem.v.mitin@yandex.ru>
2023-01-04 18:46:59 +0300

    feat: настроить запросы на несуществующий роут

--- app.js
+++ app.js
@@ -20,5 +20,8 @@ app.use((req, res, next) => {
 });
 app.use("/users", usersRouter);
 app.use("/cards", cardsRouter);
+app.use("*", (req, res) => {
+  res.status(404).send({ message: "Not available" });
+});
 
 app.listen(PORT);
