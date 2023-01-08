80479dd465288fe0e8ee5f35dc68b2e265829153
Artem <artem.v.mitin@yandex.ru>
2023-01-08 17:14:52 +0300

    feat: add helmet and express-rare-limit

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const bodyParser = require("body-parser");
 const usersRouter = require("./routes/users");
 const cardsRouter = require("./routes/cards");
 
 const app = express();
 const { PORT = 3000 } = process.env;
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 mongoose.set("strictQuery", true);
 mongoose.connect("mongodb://localhost:27017/mestodb");
 
+app.use(limiter);
+app.use(helmet());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   req.user = {
